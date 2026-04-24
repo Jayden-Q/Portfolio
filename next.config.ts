f435326bdd6608bc878d76ca4e06c8fd1070e47b
JaydenQ <officialjaydenqin@gmail.com>
2026-04-24 22:05:10 +0100

    Added 308 redirect

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.6']
+  allowedDevOrigins: ['192.168.1.6'],
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.jaydenqin.com',
+          },
+        ],
+        destination: 'https://jaydenqin.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
